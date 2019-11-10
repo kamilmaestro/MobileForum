5817a46cbb91f0a2208fbc19b3467b852b8c6404
Kamil Marnik <ksdekamil@gmail.com>
2019-11-10 16:56:10 +0100

    login activity -design

--- app/src/main/java/com/kamilmarnik/mobileforum/LoginActivity.kt
+++ app/src/main/java/com/kamilmarnik/mobileforum/LoginActivity.kt
@@ -2,11 +2,18 @@ package com.kamilmarnik.mobileforum
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var loginBtn: View
+    private lateinit var registerBtn: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginBtn = findViewById(R.id.loginBtn)
+        registerBtn = findViewById(R.id.registerBtn)
     }
 }
---
+++ app/src/main/java/com/kamilmarnik/mobileforum/api/requests.kt
@@ -0,0 +1,4 @@
+package com.kamilmarnik.mobileforum.api
+
+interface requests {
+}
